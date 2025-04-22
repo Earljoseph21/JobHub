42e7074202e868db7455e8acdd563a40e2f343b4
Earl Joseph D. Taran <156991493+Earljoseph21@users.noreply.github.com>
2025-04-22 22:46:47 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+require_once 'views/header.php';
+
+
+echo "<h2>Available Jobs</h2>";
+
+ 
+echo '<img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fstock.adobe.com%2Fsearch%2Fimages%3Fk%3Djob%2Bbackground&psig=AOvVaw0xDm1kpbFhMdtTO0KrRcqg&ust=1745318440453000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPjLuYj46IwDFQAAAAAdAAAAABAE" alt="Available Jobs" width="500" height="auto">';
+
+require_once 'views/footer.php';
