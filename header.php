794209437c6b9c33434e8e7c51b9501f29363a2f
Earl Joseph D. Taran <156991493+Earljoseph21@users.noreply.github.com>
2025-04-22 22:45:41 +0800

    Create header.php

---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Jobhub</title>
+    <link href="./res/style.css" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+   <body>
+    <div class="navbar">
+            <a href="./index.php">Home</a>
+            <a href="./post_job.php">Job</a>
+            <a href="./about.php">About</a>
+             <a href="./register.php">Register</a>
+    <hr>
+
+
+
+
